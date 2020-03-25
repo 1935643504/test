dff5de9ac6989004df1cd7fca5f72206212c99f9
1935643504 <1935643504@qq.com>
2020-03-25 18:36:17 +0800

    第一次提交到本地仓库

---
+++ src/main/java/com/hr/Gitwork01Application.java
@@ -0,0 +1,13 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitwork01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitwork01Application.class, args);
+    }
+
+}
